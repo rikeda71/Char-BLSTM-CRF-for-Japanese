d90c923c48ce5a064329db767adc44cf2033ed33
Ryuya Ikeda <rikeda71@gmail.com>
2018-11-22 21:10:23 +0900

    Edit: trainer deal with model and dataloader

--- Char-BLSTM-CRF-for-Japanese/train.py
+++ Char-BLSTM-CRF-for-Japanese/train.py
@@ -1,3 +1,4 @@
+import torch
 import torch.nn as nn
 import torch.optim as optim
 import numpy as np
@@ -7,16 +8,18 @@ from dataset import Dataset
 
 class Trainer():
 
-    def __init__(self, optimizer, net: nn.Module,
-                 num_labels: int, hidden_size: int,
-                 batch_size: int, dropout_rate: int, pad_idx: int,
-                 wordemb: np.ndarray, charemb: np.ndarray,
-                 text_path: str):
-        self.criterion = nn.CrossEntropyLoss()
-        self.optimizer = optim.Adam(params=net, lr=1e-3)
-        self.model = BLSTMCRF(num_labels, hidden_size, batch_size, dropout_rate, pad_idx, wordemb, charemb)
-        self.dataset = Dataset(text_path, wordemb, charemb)
-        self.iterator = self.dataset.return_batch(batch_size)
+    def __init__(self, optimizer,
+                 hidden_size: int, batch_size: int,
+                 dropout_rate: float, pad_idx: int,
+                 wordemb_path: str, charemb_path: str,
+                 text_path: str, save_path: str='weight.pth'):
+
+        self.batch_size = batch_size
+        self.dataset = Dataset(text_path, wordemb_path, charemb_path)
+        dim_sizes = self.dataset.return_embedding_dim()
+        num_labels = self.dataset.return_num_label_kind()
+        self.model = BLSTMCRF(num_labels, hidden_size, dropout_rate, pad_idx, dim_sizes['word'], dim_sizes['char'])
+        self.optimizer = optim.Adam(params=self.model.parameters(), lr=1e-3)
 
     def train(self, epoch_size: int=5):
         for epoch in range(epoch_size):
@@ -24,22 +27,47 @@ class Trainer():
             # loss of all of learning data
             all_loss = 0.0
 
-            for i, data in enumerate(self.iterator):
-                char, word, labels = data
+            iterator = self.dataset.return_batch(self.batch_size)
+            for i, data in enumerate(iterator):
+                word = self.dataset.WORD.vocab.vectors[data.word]
+                char = self.dataset.CHAR.vocab.vectors[data.char]
+                # maskを作成
+                mask = data.word != 1
+                mask = mask.float()
+                labels = data.label
                 x = {'word': word, 'char': char}
 
                 # reset gradient
                 self.optimizer.zero_grad()
 
-                output = self.model(x, labels)
-                
-                loss = self.criterion(output, labels)
+                loss = -torch.mean(self.model(x, labels, mask))
+
                 loss.backward()
                 self.optimizer.step()
 
                 # print loss
-                all_loss += loss.data[0]
-                if i % 2000 == 1999:
-                    print('[%d, %5d] loss: %.3f' %
-                  (epoch + 1, i + 1, all_loss / 2000))
+                all_loss += loss.tolist()
+            print('[%d, %5d] loss: %.3f' %
+                  (epoch + 1, i + 1, all_loss))
             all_loss = 0.0
+        torch.save(self.model.state_dict(), save_path)
+
+    def load(self, model_path: str):
+        self.model.load_state_dict(torch.load(model_path))
+
+    def predict(self):
+        iterator = self.dataset.return_batch(self.batch_size)
+        for i, data in enumerate(iterator):
+            word = self.dataset.WORD.vocab.vectors[data.word]
+            char = self.dataset.CHAR.vocab.vectors[data.char]
+            # maskを作成
+            mask = data.word != 1
+            mask = mask.float()
+            labels = data.label
+            x = {'word': word, 'char': char}
+            decode = self.model.decode(x, mask)
+            for a, b, c in zip(decode, data.label, data.char):
+                print('predict', a)
+                print('answer', b[:len(a)])
+                print('sentence', [self.dataset.CHAR.vocab.itos[i]
+                                   for i in c])
