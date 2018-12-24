3c6e5e675185ebd256173b482f0017522404c822
Ryuya Ikeda <rikeda71@gmail.com>
2018-12-25 00:18:11 +0900

    Add: execute samples

---
+++ exec_script.py
@@ -0,0 +1,17 @@
+import torch.optim
+from time import time
+from miner import Miner
+from src.trainer import Trainer
+from src.reporter import Reporter
+
+t = Trainer(optimizer=torch.optim.Adam, hidden_size=300, batch_size=32, pad_idx=1,
+            wordemb_path='word_vectors.txt',
+            charemb_path='char_vectors.txt',
+            train_path='train.txt', test_path='test.txt', dropout_rate=0.5)
+now = time()
+t.train(10)
+print(str(time() - now) + "秒")
+t.model.load('weight.pth')
+
+reporter = Reporter(t)
+reporter.all_report()
