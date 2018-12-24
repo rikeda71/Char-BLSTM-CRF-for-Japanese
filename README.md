d33a22a514b790f840cfef1dc05000658a0f1bd3
Ryuya Ikeda <rikeda71@gmail.com>
2018-12-25 00:50:03 +0900

    Edit: README.md

--- README.md
+++ README.md
@@ -1,4 +1,30 @@
 # Char-BLSTM-CRF-for-Japanese
 
+An Inplementation of Character based Bidirectional LSTM CRF for Japanese.
+
+This library is a library of name entity recognition (NER) for Japanese and reproduces the [S Misawa et al., 2017](http://www.aclweb.org/anthology/W17-4114) with pytorch.
+
+
+## Requirements
+
+- python3 (>=3.5)
+- pytorch1.0
+
+## Installation
+
+    pip install git+https://github.com/s14t284/Char-BLSTM-CRF-for-Japanese#egg=CBCJ
+
+
+## Usage
+
+I prepared sample codes. Please see [exec\_script](https://github.com/s14t284/Char-BLSTM-CRF-for-Japanese/brob/master/exec_script) or [exec\_sample.ipynb](https://github.com/s14t284/Char-BLSTM-CRF-for-Japanese/brob/master/exec_sample.pynb) .
+
+
 ## Reference
 - S Misawa, M Taniguchi, et al. [Character-based Bidirectional LSTM-CRF with words and characters for Japanese Named Entity Recognition](http://www.aclweb.org/anthology/W17-4114)
+
+
+## License
+
+MIT
+
--- src/dataset.py
+++ src/dataset.py
@@ -2,7 +2,6 @@ from typing import Dict
 import torch
 from torchtext import data, datasets
 from torchtext.vocab import Vectors
-import numpy as np
 
 
 class Dataset():
@@ -14,6 +13,7 @@ class Dataset():
     def __init__(self, text_path: str,
                  wordembed_path: str, charembed_path: str):
         """
+        The form of dataset
         想定しているデータセットの形
         私は白い恋人を食べました
         私  私  O
@@ -37,9 +37,18 @@ class Dataset():
         self.LABEL.build_vocab(self.dataset)
 
     def return_dataset(self):
+        """
+        return dataset variable
+        """
+
         return self.dataset
 
     def return_batch(self, batch_size: int):
+        """
+        return dataset in batches (iterator form)
+        :param batch_size: size of batches
+        """
+
         return data.BucketIterator(dataset=self.dataset,
                                    batch_size=batch_size,
                                    sort=True,
@@ -47,9 +56,17 @@ class Dataset():
                                    repeat=False)
 
     def return_embedding_dim(self) -> Dict[str, int]:
+        """
+        return size of embedding dimension (word embedding dimension and char embedding dimension)
+        """
+
         char_dim = self.CHAR.vocab.vectors.shape[1]
         word_dim = self.WORD.vocab.vectors.shape[1]
         return {'char': char_dim, 'word': word_dim}
 
     def return_num_label_kind(self) -> int:
+        """
+        return number of kind of label
+        """
+
         return len(self.LABEL.vocab.itos)
