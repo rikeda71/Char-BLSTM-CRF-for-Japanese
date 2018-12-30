59e82661925ba572576c89cf60b9479d89b44462
Ryuya Ikeda <rikeda71@gmail.com>
2018-12-30 17:32:30 +0900

    Edit: exec samples

--- exec_script.py
+++ exec_script.py
@@ -1,17 +1,13 @@
 import torch.optim
 from time import time
-from miner import Miner
-from deepjapaner.trainer import Trainer
-from deepjapaner.reporter import Reporter
+from deepjapaner.experimenter import Experimenter
 
-t = Trainer(optimizer=torch.optim.Adam, hidden_size=300, batch_size=32,
-            wordemb_path='word_vectors.txt',
-            charemb_path='char_vectors.txt',
-            train_path='train.txt', test_path='test.txt', dropout_rate=0.5)
+e = Experimenter(optimizer=torch.optim.Adam,
+                 wordemb_path='word_vectors.txt', charemb_path='char_vectors.txt',
+                 train_path='train.txt', test_path='test.txt', dev_path='test.txt',
+                 hidden_size=300, batch_size=32, epoch_size=30,
+                 )
+
 now = time()
-t.train(10)
-t.model.load('weight.pth')
+e.run(label='PRO', target='unknown', measured_val='f1_score', patience=3)
 print(str(time() - now) + "秒")
-
-reporter = Reporter(t)
-reporter.all_report()
